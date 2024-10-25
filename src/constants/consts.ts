cc8fcb6a306bf3e615f13d872a69912c4cb931ba
George <george.valuiskyi@gmail.com>
2024-10-25 16:03:26 +0300

    feat: add constants folder
    Add a folder to store constants files and
    create a TYPES file for the DI keys.

---
+++ src/constants/consts.ts
@@ -0,0 +1,8 @@
+export const TYPES = {
+    Application: Symbol.for("Application"),
+    Logger: Symbol.for("Logger"),
+    ExceptionFilter: Symbol.for("ExceptionFilter"),
+    ServerEventsHandler: Symbol.for("ServerEventsHandler"),
+    UrlController: Symbol.for("UrlController"),
+    IdService: Symbol.for("IdService"),
+};
